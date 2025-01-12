07b1594d0944b25edbf309461521392da9b0c1f6
hyeryunYou <hyeryun1108@naver.com>
2025-01-13 06:05:32 +0900

    이용자 및 대여자 회원가입 성공 페이지 1차 작성완료 (패드백 전)

--- src/pages/owner/owner-success.jsx
+++ src/pages/owner/owner-success.jsx
@@ -1,15 +1,19 @@
 import '../successpage.css';
 import NextButton from '../../components/button';
 
-export default function OwnerSuccess() {
+export default function UserSuccess() {
     return (
         <div className="success-container">
-            <main className="success-content">
-                <p>회원가입에</p>
-                <h1>성공했어요!</h1>
-                <NextButton text="룸 등록하기"/> {/*다음 버튼 공용 컴포넌트 사용*/}
-                <button className="skip-btn">건너뛰기</button>
+            <main className="success-center"> {/*가운데 콘텐츠 영역*/}
+                <div>
+                    <p>회원가입에</p>
+                    <h1>성공했어요!</h1>
+                </div>
+                <div>
+                    <NextButton text="룸 등록하기"/> {/*다음 버튼 공용 컴포넌트 사용*/}
+                    <button className="skip-btn">건너뛰기</button>    
+                </div>
             </main>
-        </div>
+        </div>  
     );
 }
